6224dde29acc914b58842e94fc64168288fd8ed8
ryutaro06 <nyukon.ryu305@icloud.com>
2022-10-14 23:20:40 +0900

    アコーディオン機能の追加 20221014 23:00

--- javascript_practice/js/javascript.js
+++ javascript_practice/js/javascript.js
@@ -337,6 +337,7 @@ var swiper2 = new Swiper(".mySwiper_thumbs_gallery_loop2", {
     swiper: swiper,
   },
 });
+
 // LodingのJS
 //テキストのカウントアップの設定
 var bar = new ProgressBar.Line(splash_text, {//id名を指定
@@ -367,3 +368,62 @@ var bar = new ProgressBar.Line(splash_text, {//id名を指定
 bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
   $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
 }); 
+
+// 一番上へ
+document.getElementById('upScroll').addEventListener('click', () => {
+  var a = document.documentElement;
+  var y = a.scrollHeight - a.clientHeight;
+  window.scroll({top: 0});
+});
+
+// 一番下へ
+document.getElementById('downScroll').addEventListener('click', () => {
+  var a = document.documentElement;
+  var y = a.scrollHeight - a.clientHeight;
+  window.scroll(0, y);
+});
+// scrollHeight：要素の中身（ページ）の高さ
+// clientHeight：表示域（ブラウザ）の高さ
+
+// tab_change_area
+//任意のタブにURLからリンクするための設定
+function GethashID (hashIDName){
+  if(hashIDName){
+    //タブ設定
+    $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
+      var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得 
+      if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+        var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+        $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+        $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+        //表示させるエリア設定
+        $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+        $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加 
+      }
+    });
+  }
+}
+
+//タブをクリックしたら
+$('.tab a').on('click', function() {
+  var idName = $(this).attr('href'); //タブ内のリンク名を取得  
+  GethashID (idName);//設定したタブの読み込みと
+  return false;//aタグを無効にする
+});
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+    $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+    $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
+  var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+  GethashID (hashName);//設定したタブの読み込み
+});
+
+// アコーデオン
+$(function(){
+  //クリックで動く
+  $('.nav-open').click(function(){
+    $(this).toggleClass('active');
+    $(this).next('nav').slideToggle();
+  });
+});
